dc5a1d0d7dee2ecf4e666dfdab9a8fa12cf624f2
cubingarea <cubingarea@gmail.com>
2025-05-22 14:04:28 +0800

    update experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-function toggleMenu() {
-  const navMenu = document.querySelector("nav .nav-container ul");
-  navMenu.classList.toggle("show");
-}
+// Mobile menu toggle
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenuButton.addEventListener("click", function () {
+  mobileMenu.classList.toggle("hidden");
+});
+
+// Close mobile menu when clicking on a menu item
+const mobileMenuItems = document.querySelectorAll("#mobile-menu a");
+mobileMenuItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    mobileMenu.classList.add("hidden");
+  });
+});
